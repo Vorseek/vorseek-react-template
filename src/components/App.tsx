61da547d5dc67a96ad7d1da0f7379b28f60b50bd
Mikhail Belov <m.belov@3commas.io>
2023-01-10 19:51:12 +0400

    update App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,7 +2,7 @@ import { Button } from 'src/components/Button';
 
 export const App = () => (
   <section>
-    <h1>Create React App</h1>
+    <h1>Create React App.</h1>
 
     <Button />
   </section>
